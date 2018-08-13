276da8dc6015089e7154a977556dd283bad3390c
Joshua Hall <josh.v.hall@gmail.com>
2018-08-13 21:59:12 +1000

    Finished the jpg converter so it now puts all images that are broekn in a discard folder, removes whitespace in the name, and converts to jpg.

--- ImageDatasetTools/ConvertToJpg.py
+++ ImageDatasetTools/ConvertToJpg.py
@@ -4,20 +4,29 @@
 import sys
 import os
 from PIL import Image
+import shutil
+ 
+discardFolderPath = ""
+def move(src, dest):
+    shutil.move(src, dest)
+
 #converts images to jpg format and cleans path e.g. removes white space.
 
+
 def main(argv):
-    rootFolderPath = argv[0]
-    #make a "clean directory"
-    #cleanPath = os.path.join(rootFolderPath, "clean")
-    #if not os.path.exists(cleanPath):
-    #    os.makedirs(cleanPath)
-    for subdir, dirs, files in os.walk(rootFolderPath):
-        for name in files:
-            imagePath = os.path.join(subdir, name)
-            #strip white space
-            print(imagePath)
-            convertTojpg(imagePath)
+	rootFolderPath = argv[0]
+	#create a discard folder 
+	discardFolderPath = os.path.join(rootFolderPath, "discard")
+	if not os.path.exists(discardFolderPath):
+		os.makedirs(discardFolderPath)
+
+	
+	for subdir, dirs, files in os.walk(rootFolderPath):
+		for name in files:
+			imagePath = os.path.join(subdir, name)
+			#strip white space
+			print(imagePath)
+			convertTojpg(imagePath, rootFolderPath)
         #for name in dirs:
         #    print(os.path.join(rootFolderPath, name))
 
@@ -28,46 +37,52 @@ def main(argv):
 
     #pngTojpg(inputfile)
 
-def convertTojpg(inputfile):
+def convertTojpg(inputfile, rootFolder):
 
     finalPath = ""
+    discardFolderPath = os.path.join(rootFolder, "discard")
     try:
         #open the file
         image = Image.open(inputfile)
         #confirm the image is not corrupted
         image.verify() # verify that it is, in fact an image
     except (IOError, SyntaxError) as e:
-        print('Bad file:', inputfile) # print out the names of corrupt files
+        print('!!!!!!!!! Bad file:', inputfile) # print out the names of corrupt files
+        #move the file to the discard folder, that was already created
+        (name, extension) = os.path.splitext(inputfile)
+        deletePath = os.path.join(discardFolderPath, os.path.basename(inputfile))
+        #print("#### "+ deletePath)
+        move(inputfile, deletePath)
 
     try:
         # Attempt to open an image file
         filepath = inputfile
-
-        #remove whitespace in the name
-        cleanPath = filepath.replace(" ","")
-        os.rename(filepath, cleanPath)
-
         #open the file
-        image = Image.open(cleanPath)
+        image = Image.open(filepath)
+        #remove whitespace in the name
+        #cleanPath = filepath.replace(" ","")
+        #os.rename(filepath, cleanPath)
+
+        #convert to rgb just in case
+        image = image.convert('RGB')
         #delete the
         # Resize the image
         #image = image.resize(thumbnail_size, Image.ANTIALIAS)
-        #Split our original filename into name and extension
-        (name, extension) = os.path.splitext(cleanPath)
 
+        #save
+        #image.save(filepath)
+        #Split our original filename into name and extension
+        (name, extension) = os.path.splitext(filepath)
         #save image wth valid jpg extension
         finalPath = name + '.jpg'
-        image.save(finalPath)
+        finalPath = finalPath.replace(" ","")
+        os.rename(filepath, finalPath)
         # Save the thumbnail as "(original_name)_thumb.png"
+
     except IOError as e:
     # Report error, and then skip to the next argument
         print ("Problem opening", filepath, ":", e)
 
-    
-
-    
-
-        
 
 if __name__=="__main__":
     main(sys.argv[1:])
